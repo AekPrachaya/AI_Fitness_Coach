1a6a77750d6a624c23e6d042a7b1258d3e3b59b4
Realtoxkulus <pathanin.pra@dome.tu.ac.th>
2026-04-02 14:59:10 +0700

    Create typography-design-system

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import '../theme/typography_preview.dart';
 
 final routerProvider = Provider<GoRouter>((ref) {
   return GoRouter(
     initialLocation: '/',
     routes: [
       GoRoute(path: '/', builder: (context, state) => const Scaffold()),
+      GoRoute(
+        path: '/typography-preview',
+        builder: (context, state) => const TypographyPreview(),
+      ),
     ],
   );
 });
